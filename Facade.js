c980dbc0e055f6dff634f6fae32f0e6fd5f0d742
Kirill <kupuk132@gmail.com>
2020-09-30 06:32:38 +0300

    write Facade

---
+++ Facade.js
@@ -0,0 +1,56 @@
+class Conveyor {
+  setBody() {
+    console.log('Body set!')
+  }
+  getEngine() {
+    console.log('Dismantle engine!')
+  }
+  setEngine() {
+    console.log('Engine set!')
+  }
+  getInterior() {
+    console.log('Dismantle interior')
+  }
+  setInterior() {
+    console.log('Interior added!')
+  }
+  setWheels() {
+    console.log('Wheels!')
+  }
+  addElectronics() {
+    console.log('Added electronics!')
+  }
+  paint() {
+    console.log('Car paint!')
+  }
+}
+
+class ConveyorFacade {
+  constructor(car) {
+    this.car = car
+  }
+
+  assembleCar() {
+    this.car.setBody()
+    this.car.setEngine()
+    this.car.setInterior()
+    this.car.setWheels()
+    this.car.addElectronics()
+    this.car.paint()
+  }
+
+  changeEngine() {
+    this.car.getEngine()
+    this.car.setEngine()
+  }
+
+  changeInterior() {
+    this.car.getInterior()
+    this.car.setInterior()
+  }
+}
+
+const conveyor = new ConveyorFacade(new Conveyor())
+let car = conveyor.assembleCar()
+car = conveyor.changeEngine()
+car = conveyor.changeInterior()
